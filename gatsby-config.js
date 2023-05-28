2030601b910d6b27205f815a58a25be6d65fd0a9
gpeshans <pesanski.goran@gmail.com>
2023-05-28 10:40:13 +0200

    latest and greatest and back to the basics

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,39 +8,28 @@ module.exports = {
     siteUrl: 'https://www.stellararchitecture.com',
   },
   plugins: [
-    'gatsby-plugin-netlify-cms',
     'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'test',
       },
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
         icon: 'src/images/icon.png',
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'projects',
-        path: `${__dirname}/src/pages/projects`,
-      },
-    },
-    `gatsby-transformer-remark`,
   ],
 };
